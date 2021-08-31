ade7abdbbc79cf7e9962f3415b81d7bacc60e525
Samy Zogeyb <contact@samyzogeyb.com>
2021-08-31 06:53:15 +0400

    ready for deployment on gh pages

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider as StoreProvider } from "react-redux";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import App from "./App";
 import store from "./store/rootReducer";
 import "./styles/main.scss";
