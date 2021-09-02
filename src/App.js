66a7f0f37f3ad1c7e46e492c34a04246d873f895
Samy Zogeyb <contact@samyzogeyb.com>
2021-09-03 00:18:06 +0400

    removed text notice

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import Box from "./components/layout/Box";
-import Center from "./components/layout/Center";
 import Main from "./components/sectioning/Main/Main";
 import Cartpage from "./routes/Cartpage/Cartpage";
 
@@ -11,25 +9,6 @@ function App() {
 		<>
 			<Header />
 			<Main>
-				<Switch>
-					<Route exact path="/">
-						<Center style={{ height: "90vh", width: "50%" }}>
-							<Box style={{ fontSize: "2em" }}>
-								Hello 👋! Before taking a look at my implementation of the cart, please take some time
-								and look at the{" "}
-								<a
-									rel="noreferrer"
-									target="_blank"
-									style={{ textDecoration: "underline" }}
-									href="https://github.com/SamyZog/optimax-test#readme"
-								>
-									README
-								</a>{" "}
-								section of this repo to have a better outlook.
-							</Box>
-						</Center>
-					</Route>
-				</Switch>
 				<Switch>
 					<Route path="/cart">
 						<Cartpage />
